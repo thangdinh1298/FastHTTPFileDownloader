04dcadf97867ab4be7192684e4f09fef2bc5989e
thangdinh1298 <thangdinh.umbala@gmail.com>
2019-10-07 22:34:04 +0700

    Refactor: moved Download Thread to download entry and pause/resume function to download entry

--- src/Downloaders/MultiThreadedDownloader.java
+++ src/Downloaders/MultiThreadedDownloader.java
@@ -14,14 +14,12 @@ import java.util.concurrent.Future;
 
 public class MultiThreadedDownloader extends DownloadEntry{
     private static final long serialVersionUID = -7221155498655620062l;
-    private transient Future[] futures;
-
     public MultiThreadedDownloader(URL url, Long fileSize, String downloadDir, String fileName) throws IOException{
         super(url, downloadDir, fileName, true);
         this.fileSize = fileSize;
         this.threadNum = Configs.THREAD_NUM;
         //todo: this is a temporay solution to fix NullPointerException when entries are loaded from files and futures are not initialized. For a sound solution, State should be implemented
-        futures = new Future[this.threadNum];
+//        futures = new Future[this.threadNum];
     }
 
     @Override
@@ -33,49 +31,21 @@ public class MultiThreadedDownloader extends DownloadEntry{
         }
     }
 
-    @Override
-    public void resume() throws OperationNotSupportedException {
-    }
-
-    @Override
-    public void pause() throws OperationNotSupportedException{
-        synchronized (this){
-            for(Future f: this.futures){
-                if (f != null && !f.isCancelled() && !f.isDone()) f.cancel(true);
-            }
-        }
-
-        // wait for all jobs to enter terminated state
-        for(int i = 0; i < this.futures.length; i++){
-            try {
-                if (this.futures[i] != null) this.futures[i].get();
-
-            } catch (InterruptedException e) {
-//                System.out.println("Job cancelled mid execution");
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
-            } catch (CancellationException e){
-                System.out.println("Job cancelled");
-//                e.printStackTrace();
-            }
-        }
-    }
-
     private void download() throws IOException {
         this.setState(State.DOWNLOADING);
-        futures = new Future[this.threadNum];
+        if(this.futures == null){
+            futures = new Future[this.threadNum];
+        }
 
         long segmentSize = this.fileSize / this.threadNum;
-        long leftOver = this.fileSize % this.threadNum;
-        long chunkStartByte = 0;
+        long chunkStartByte = 0, startByte = 0, endByte = 0;
         for(int i = 0; i < this.threadNum; i++){
-            long bytesDownloaded =  new File(String.format("%s/%s%d", downloadDir, fileName, i)).length();
+            long bytesDownloaded =  new File(String.valueOf(Paths.get(this.downloadDir, this.fileName + i))).length();
 //            System.out.println("==============================THREAD " + i + "==================================");
 //            System.out.println("Numbytes downloaded for thread " + i + " is " + bytesDownloaded);
-            long startByte = chunkStartByte + bytesDownloaded;
-            long chunkSize = segmentSize - bytesDownloaded;
-            if (i == this.threadNum - 1) chunkSize += leftOver;
+            startByte = chunkStartByte + bytesDownloaded;
+            endByte = chunkStartByte + segmentSize - 1;
+            if (i == this.threadNum - 1) endByte = this.fileSize - 1;
 //            System.out.println(startByte + " " + chunkSize);
 //
 //            System.out.println("Now downloading from " + startByte + " to " + (startByte + chunkSize - 1));
@@ -83,9 +53,9 @@ public class MultiThreadedDownloader extends DownloadEntry{
 //            System.out.println("================================================================");
 
             chunkStartByte += segmentSize;
-            if (chunkSize == 0) continue;
+            if (startByte >= endByte) continue;
 
-            futures[i] = Controller.getInstance().getExecutorService().submit(new DownloadThread(chunkSize, startByte, i));
+            futures[i] = Controller.getInstance().getExecutorService().submit(new DownloadThread(startByte, endByte, i));
             System.out.println("Submitting task " + i);
 
         }
@@ -139,67 +109,4 @@ public class MultiThreadedDownloader extends DownloadEntry{
         }
     }
 
-    private class DownloadThread implements Runnable {
-        private long startByte;
-        private long chunkSize; //num bytes to download including the start byte
-        private int threadID;
-
-        public DownloadThread(long chunkSize, long startByte, int threadID) {
-            this.chunkSize = chunkSize;
-            this.startByte = startByte;
-            this.threadID = threadID;
-        }
-
-        /*
-            todo: Figureout a way to propagate incompleted downloads downward in the call stack
-         */
-        @Override
-        public void run() {
-//            System.out.println("Thread " + this.threadID + " is downloading from " + this.startByte + " to " + (this.startByte + this.chunkSize - 1) );
-            HttpURLConnection conn = null;
-            InputStream is = null;
-            OutputStream os = null;
-
-            try {
-                conn = (HttpURLConnection) downloadLink.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty( "charset", "utf-8");
-                conn.setRequestProperty("Content-Language", "en-US");
-                conn.setRequestProperty("Range", "bytes=" + this.startByte  + "-" + (this.startByte + this.chunkSize - 1));
-                conn.connect();
-
-                is = conn.getInputStream();
-
-                os = new BufferedOutputStream(new FileOutputStream(String.valueOf(Paths.get(downloadDir, fileName + this.threadID)), true));
-                int c;
-                long count = 0;
-                while((c = is.read()) != -1 && !Thread.interrupted()){
-                    count++;
-                    os.write(c);
-                }
-                System.out.println("Thread " + this.threadID + " downloaded "  + count + " bytes");
-                os.flush();
-
-            } catch (IOException e) {
-                System.out.println(e.getMessage());
-            } finally{
-//                System.out.println("Interrupted");
-                if (conn != null) conn.disconnect();
-                if (is != null) {
-                    try {
-                        is.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (os != null) {
-                    try {
-                        os.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
 }
