ae569556f311cd734de0c8c7071056e6bbedaf21
hieule <leconghieu1998@gmail.com>
2019-10-20 08:13:32 +0700

    fix cant delete symlink

--- uninstall.sh
+++ uninstall.sh
@@ -3,4 +3,4 @@ BUILD_DIR="build"
 DAEMON_SCRIPT="daemon"
 
 [ -d "$BUILD_DIR" ] && rm -r "$BUILD_DIR"
-[ -f ~/.local/bin/"$DAEMON_SCRIPT" ] && rm ~/.local/bin/"$DAEMON_SCRIPT"
+[ -L ~/.local/bin/"$DAEMON_SCRIPT" ] && rm ~/.local/bin/"$DAEMON_SCRIPT"
