03d8aabb10234a9d78bcb90d99d4df70a7ea8402
hieule <leconghieu1998@gmail.com>
2019-10-30 22:53:05 +0700

    add pair and time formatter

---
+++ src/Util/Pair.java
@@ -0,0 +1,13 @@
+package Util;
+
+public class Pair<T, S> {
+    public T first;
+    public S second;
+
+    public Pair(){}
+
+    public Pair(T t, S s){
+        this.first = t;
+        this.second = s;
+    }
+}
